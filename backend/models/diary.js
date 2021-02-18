a2fd528292bf9ea2b62427946fb4bf5cb9b91a27
Nhan Luong <nhanlt18@gmail.com>
2021-02-18 17:00:59 +0700

    set up model schema

---
+++ backend/models/diary.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const diarySchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Diary = mongoose.model('User', diarySchema);
+
+export default Diary;
